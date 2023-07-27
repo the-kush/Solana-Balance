5c7303a96e26d3587cc2784b9ec6ee9a120307ea
Kush Singh <kushsingh@Kushs-MacBook-Air.local>
2023-07-27 13:14:38 +0530

    Demo commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,27 @@
+import * as web3 from '@solana/web3.js'
 import type { NextPage } from 'next'
 import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 import AddressForm from '../components/AddressForm'
 import * as Web3 from '@solana/web3.js'
+import { info } from 'console'
 
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
   const [address, setAddress] = useState('')
+  const [isExecutable, setIsExecutable] = useState(false);
 
   const addressSubmittedHandler = (address: string) => {
     try {
       setAddress(address)
-      const key = new Web3.PublicKey(address)
-      const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
+      const key = new web3.PublicKey(address)
+      const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
       connection.getBalance(key).then(balance => {
-        setBalance(balance / Web3.LAMPORTS_PER_SOL)
+        setBalance(balance / web3.LAMPORTS_PER_SOL)
+      })
+
+      connection.getAccountInfo(key).then(info => {
+        setIsExecutable(info?.executable ?? false);
       })
     } catch (error) {
       setAddress('')
